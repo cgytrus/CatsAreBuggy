ce1ff39517e1aa7136195e1fd571ab75a007a25f
ConfiG <cgytrus@cgyt.ru>
2021-10-25 21:22:02 +0300

    disallow multiple instances of the same fix

--- src/Fixes/BaseFix.cs
+++ src/Fixes/BaseFix.cs
@@ -1,13 +1,21 @@
-﻿using BepInEx.Configuration;
+﻿using System;
+using System.Linq;
+
+using BepInEx.Configuration;
 
 using CalApi.Patches;
 
 namespace CatsAreBuggy.Fixes {
     public abstract class BaseFix : IPatch {
         protected static bool enabled { get; private set; }
+
         protected BaseFix(string description) {
+            string key = GetType().FullName;
+            if(CatsAreBuggyPlugin.instance.Config.Select(conf => conf.Key.Key).Contains(key))
+                throw new InvalidOperationException("Tried loading a fix which is already loaded.");
+
             ConfigEntry<bool> config =
-                CatsAreBuggyPlugin.instance.Config.Bind("Fixes", GetType().FullName, true, description);
+                CatsAreBuggyPlugin.instance.Config.Bind("Fixes", key, true, description);
             enabled = config.Value;
             config.SettingChanged += (_, _) => { enabled = config.Value; };
         }
