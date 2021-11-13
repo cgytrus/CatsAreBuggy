122b3f0bf26559a3ad54706de14b47a733d6f564
ConfiG <cgytrus@cgyt.ru>
2021-11-13 16:51:10 +0300

    internal cleanup stuff - new csproj format - .net framework 4.7.1 - use nuget libs where possible - c# 10 - nullables

--- src/CatsAreBuggyPlugin.cs
+++ src/CatsAreBuggyPlugin.cs
@@ -2,14 +2,14 @@
 
 using CalApi.API;
 
-namespace CatsAreBuggy {
-    [BepInPlugin("mod.cgytrus.plugins.calBugs", "Cats are Buggy", "0.1.2")]
-    [BepInDependency("mod.cgytrus.plugins.calapi", "0.2.1")]
-    public class CatsAreBuggyPlugin : BaseUnityPlugin {
-        public static CatsAreBuggyPlugin instance { get; private set; }
-        private void Awake() {
-            instance = this;
-            Util.ApplyAllPatches();
-        }
-    }
+namespace CatsAreBuggy;
+
+[BepInPlugin("mod.cgytrus.plugins.calBugs", "Cats are Buggy", "0.1.3")]
+[BepInDependency("mod.cgytrus.plugins.calapi", "0.2.2")]
+public class CatsAreBuggyPlugin : BaseUnityPlugin {
+    public static CatsAreBuggyPlugin? instance { get; private set; }
+
+    public CatsAreBuggyPlugin() => instance = this;
+
+    private void Awake() => Util.ApplyAllPatches();
 }
