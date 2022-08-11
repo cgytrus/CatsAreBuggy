bda0316561e4fc52cbf02d26954e5bd62a4eba53
ConfiG <cgytrus@cgyt.ru>
2022-08-11 18:50:35 +0300

    it's 0.2.7

--- CatsAreBuggy/src/CatsAreBuggyPlugin.cs
+++ CatsAreBuggy/src/CatsAreBuggyPlugin.cs
@@ -5,7 +5,7 @@ using CalApi.API;
 namespace CatsAreBuggy;
 
 [BepInPlugin("mod.cgytrus.plugins.calBugs", "Cats are Buggy", "0.1.4")]
-[BepInDependency("mod.cgytrus.plugins.calapi", "0.2.8")]
+[BepInDependency("mod.cgytrus.plugins.calapi", "0.2.7")]
 public class CatsAreBuggyPlugin : BaseUnityPlugin {
     public static CatsAreBuggyPlugin? instance { get; private set; }
 
